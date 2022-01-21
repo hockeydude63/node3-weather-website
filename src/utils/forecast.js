53e2500440ddafcbfc4b48d104bf0b53553f50b1
Stewart Terry <44471071+hockeydude63@users.noreply.github.com>
2022-01-21 15:15:17 -0700

    Adding more weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out.  It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. Wind is from the ' + body.current.wind_dir + ' at ' + body.current.wind_speed + '. It is currently ' + body.current.temperature + ' degrees out.  It feels like ' + body.current.feelslike + ' degrees out.')
         }
     })
 }
